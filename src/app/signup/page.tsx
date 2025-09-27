86aa523309dd98e70ff5aad9293c891ba0afbf6d
VikumKarunathilake <vikumbk@yahoo.com>
2025-09-27 16:05:29 +0530

    Update login and signup forms to use a new image and add Google login button with SVG icon

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -13,7 +13,7 @@ export default function SignupPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src="/image.png"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
